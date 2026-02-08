a88bdbf8e1356cac39844140e5856e4e91f44f74
Raj Shekhar <rajshekharpcs@gmail.com>
2026-02-08 15:59:55 +0530

    fix: point update URL to own fork

--- telegram.go
+++ telegram.go
@@ -49,7 +49,7 @@ func updateCCC(config *Config, chatID, threadID int64, offset int) {
 	sendMessage(config, chatID, threadID, "🔄 Updating ccc...")
 
 	binaryName := fmt.Sprintf("ccc-%s-%s", runtime.GOOS, runtime.GOARCH)
-	downloadURL := fmt.Sprintf("https://github.com/kidandcat/ccc/releases/latest/download/%s", binaryName)
+	downloadURL := fmt.Sprintf("https://github.com/rsh3khar/ccc/releases/latest/download/%s", binaryName)
 
 	resp, err := http.Get(downloadURL)
 	if err != nil {
